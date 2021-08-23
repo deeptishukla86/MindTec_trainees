f324c26e0428226b6c7fa4b61e2ae617c30f9f70
hritammondal <hritammondal@outlook.com>
2021-08-23 17:27:27 +0530

    Init Commit

---
+++ Hritam_Reverse/Hritam_Reverse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hritam_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int num;
+			int reverse = 0;
+
+			Console.WriteLine("Enter the number : ");
+			num = int.Parse(Console.ReadLine());
+
+			while (num > 0)
+			{
+				reverse = reverse * 10 + num % 10;
+				num = num / 10;
+			}
+
+			Console.WriteLine("Reversed number : " + reverse);
+			Console.ReadKey();
+		}
+    }
+}
