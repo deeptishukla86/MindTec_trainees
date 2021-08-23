6bc7abec5874aa6547a9467c12f03b5e03c01351
soumik-07 <soumikkahali@gmail.com>
2021-08-23 17:23:42 +0530

    Reverse num

---
+++ Soumik_rev/Soumik_rev/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soumik_rev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, reverse = 0, rem;
+            Console.WriteLine("Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while(num>0)
+            {
+                rem = num % 10;
+                reverse = (reverse * 10) + rem;
+                num = num / 10;
+
+            }
+
+            Console.WriteLine("Reverse number is " + reverse);
+            Console.ReadLine();
+        }
+    }
+}
