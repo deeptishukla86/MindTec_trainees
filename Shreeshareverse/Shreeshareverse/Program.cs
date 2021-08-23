37f22f7481d92dfdd6aa47153ab74265b95a9cac
Shreeshanadoor <Shreeshanadoor>
2021-08-23 17:33:17 +0530

    reverse

---
+++ Shreeshareverse/Shreeshareverse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Shreeshareverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int reverse = 0, rem;
+            Console.WriteLine("Enter a number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            while(n!=0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n = n / 10;
+            }
+            Console.WriteLine("eversed Number:" + reverse);
+        }
+    }
+}
