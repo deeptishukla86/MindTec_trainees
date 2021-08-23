b60353b187c7bd00661a32f44eca8df00e0711bc
thisisayush2912@gmail.com <thisisayush2912@gmail.com>
2021-08-23 17:22:04 +0530

    NewProg

---
+++ Ayush_Reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AyushGupta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, rev = 0, rem;
+            Console.Write("Enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                rev = rev * 10 + rem;
+                n /= 10;
+            }
+            Console.WriteLine("Reversed Number: " + rev);
+            Console.ReadKey();
+        }
+    }
+}
