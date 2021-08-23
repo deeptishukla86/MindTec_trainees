10f698b7c3c55b7d09c0c19a05141df54dcebf8f
ChandanaVijay <chandanavk96@gmail.com>
2021-08-23 17:50:47 +0530

    revnum

---
+++ Chandana_Vijay_ReverseNum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Chandana_Vijay_ReverseNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rev = 0, n, rem;
+            Console.WriteLine("Enter the number");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                rev = rev * 10 + rem;
+                n /= 10;
+            }
+            Console.WriteLine("Reversed Number: " + rev);
+        }
+    }
+}
