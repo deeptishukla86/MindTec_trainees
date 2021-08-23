c27652d93a5ba2773d4a0392db553b034ff350b6
saptarshibanerjee8335 <1sappy8335@gmail.com>
2021-08-23 17:26:33 +0530

    firsprogram

---
+++ Saptarshi_Banerjee/Reverse_Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int n = int.Parse(Console.ReadLine());
+            int rev = 0;
+            while(n>0)
+            {
+                int rem = n % 10;
+                rev = (rev * 10) + rem;
+                n = n / 10;
+            }
+            Console.WriteLine("Reverse No. is {0}", rev);
+            Console.ReadLine();
+        }
+    }
+}
