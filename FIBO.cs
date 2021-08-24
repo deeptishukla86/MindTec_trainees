f9a42691c6eb15b091d103d80ca93994c924589f
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-25 01:17:14 +0530

    fibonacci series

---
+++ FIBO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, i, n;
+            Console.Write("Enter the number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");
+            for (i = 2; i < n; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.ReadLine();
+        }
+    }
+}
