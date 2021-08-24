83564c7237013c1129214b850b2382bc0c793148
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-25 01:27:16 +0530

    1D array

---
+++ 1d_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static string[] ar = new string[] { "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY" };
+        static void Main(string[] args)
+        {
+            
+         
+            foreach (var i in ar)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
