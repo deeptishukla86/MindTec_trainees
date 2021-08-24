7069eaa25d89c10715fa614c5a40ec4bd27086d9
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-25 01:16:40 +0530

    factorial of a number

---
+++ FACT.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, f = 1, n;
+            Console.WriteLine("Enter the value of n:");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i=1;i<=n;i++)
+            {
+                f = f * i;
+            }
+            Console.WriteLine("Factorial of " +n +": "+f);
+            Console.ReadLine();
+        }
+    }
+}
