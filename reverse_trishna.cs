6dea18d2bd6fd3ed78d96ca5f763debe96e7e13d
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-24 09:05:53 +0530

    revers_of a number

---
+++ reverse_trishna.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, rev = 0, rem;
+            Console.WriteLine("enter the number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            while(n!=0)
+            {
+                rem = n % 10;
+                rev = rem + rev * 10;
+                n = n / 10;
+            }
+            Console.WriteLine("Reverse of the " + n + "is:" + rev);
+            Console.ReadLine();
+        }
+    }
+}
