45b8f8a6424e257d4bccc5552bc5e7c6021c3e05
AdityaMogera <aditya.moger@gmail.com>
2021-08-24 11:35:07 +0530

    textfile

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch;
+            Console.Write("Enter your choice: ");
+            ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 10:
+                    Console.WriteLine("You have choosen number 10");
+                    break;
+                case 20:
+                    Console.WriteLine("You have choosen number 20");
+                    break;
+                case 30:
+                    Console.WriteLine("You have choosen number 30");
+                    break;
+                default:
+                    Console.WriteLine("Please choose correct number!!!");
+                    break;
+            }
+        }
+    }
+
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int sum = 0;
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= a; i++)
+            {
+                sum = sum + i;
+            }
+                Console.WriteLine("sum of natural number  is:" + sum);
+            
+        }
+        
+    }
+   
+}
+
