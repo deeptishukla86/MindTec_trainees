71ee7ab96d69cdcc39b3918cc5b293b3602a5101
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-25 01:25:28 +0530

    static method example

---
+++ static_method.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examplestatic
+{
+    class Program
+    {
+        public static void add(int a ,int b)
+        {
+
+            Console.WriteLine("sum:" + (a + b));
+
+
+        }
+        public static void sub(int a,int b)
+        {
+
+            Console.WriteLine("sub:" + (a - b));
+
+        }
+        public static void multiply(int a, int b)
+        {
+
+            Console.WriteLine("multi:" + (a * b));
+
+        }
+        public static void div(int a,int b)
+        {
+
+            Console.WriteLine("div:" + (a / b));
+
+        }
+        static void Main(string[] args)
+        {
+            add(2, 2);
+            sub(2, 2);
+            multiply(2, 2);
+            div(2, 2);
+            Console.ReadLine();
+        }
+    }
+}
