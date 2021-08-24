13d3b5f5fd887a267c2b1e6a85b2864ba4b0378a
AdityaMogera <aditya.moger@gmail.com>
2021-08-24 22:41:53 +0530

    rev

---
+++ ConsoleApp2/ConsoleApp2/Class2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void main(string[] args)
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.WriteLine("Reversed Number: " + reverse);
+        }
+    }
+}
+
