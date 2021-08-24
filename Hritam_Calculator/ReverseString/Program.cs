0b694ef46e1b016e99c3ae577b78168d7aa79c4d
hritammondal <hritammondal@outlook.com>
2021-08-24 11:33:50 +0530

    Other programs

---
+++ Hritam_Calculator/ReverseString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(RevString(str));
+        }
+
+        private static string RevString(string str)
+        {
+            string revStr = "";
+            int length = str.Length - 1;
+            while (length >= 0)
+            {
+                revStr = revStr + str[length];
+                length--;
+            }
+            return revStr;
+        }
+    }
+}
---
+++ Hritam_Calculator/TranglePattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TranglePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= val; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write("");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
