46105877776c9d093879c4554c20f7fe0c132c4f
Sujaan Chakraborty <sujaan.chakraborty@aot.edu.in>
2021-08-24 09:10:01 +0530

    Add files via upload

---
+++ Sujaan/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sujaan_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter a number to be reversed: ");
+            int i = Convert.ToInt32(System.Console.ReadLine());
+            int a=1, b=0, x=i;
+            while (x > 0)
+            {
+                b = b + a * (x % 10);
+                x = x / 10;
+                a = a * 10;
+            }
+            System.Console.WriteLine("The reverse of " + i + " is " + b);
+            System.Console.ReadLine();
+        }
+    }
+}
