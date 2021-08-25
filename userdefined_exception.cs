544589af23bd7331299063923c217f04fe1b1ded
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-26 00:50:11 +0530

    userdefined exception handling

---
+++ userdefined_exception.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace userdefined_exc
+{
+    public class bank
+    {
+      
+
+        public void cal(double balance)
+        {
+            double bal = 0.0;
+            while(balance!=0)
+            {
+                Console.WriteLine("enter the amont that u want to debit:");
+                double amn = Convert.ToInt32(Console.ReadLine());
+
+                if (amn <=balance)
+                {
+                    bal = balance - amn;
+                    Console.WriteLine("!!......AMOUNT DEBITED SUCCESFULLY......!!");
+                    Console.WriteLine("REMAINING BALANCE:  "+bal+" ");
+
+                }
+                else
+                {
+                    throw (new account("!!!!............INSUFFICIENT BALANCE..........!!!!"));
+                }
+                balance = balance - amn;
+            }
+            
+        }
+    }
+    public class account: Exception
+    {
+        public account(string Msg) : base(Msg)
+    {
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the amount.....:");
+            double bal = Convert.ToInt32(Console.ReadLine());
+            bank b = new bank();
+            try
+            {
+                b.cal(bal);
+            }
+            catch(account e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
