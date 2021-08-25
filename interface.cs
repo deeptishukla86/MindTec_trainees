c5d84076e07f6bf5808370ac316babbadd179a61
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-26 00:46:37 +0530

    using interface

---
+++ interface.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @interface
+{
+    interface inter
+    {
+        void even();
+        void odd();
+
+
+    }
+
+
+
+
+
+    public class exe : inter
+    {
+
+        public void even()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("this is even");
+            Console.WriteLine("this is two");
+            Console.WriteLine("this is four");
+
+        }
+        public void odd()
+        {
+            Console.WriteLine("this is one");
+            Console.WriteLine("this is three");
+            Console.WriteLine("this is five");
+
+        }
+       
+    }
+
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            exe t = new exe();
+
+            t.odd();
+            t.even();
+            
+            Console.ReadLine();
+
+        }
+    }
+}
+
