57dc7b28d0c7deee7875a706a5564b96e2a150ff
suro99 <89373189+suro99@users.noreply.github.com>
2021-08-25 15:12:28 +0530

    Bank_Transaction

---
+++ Bank_Transaction/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Transaction
+{
+    public class Bank
+    {
+        public void transaction(int bal)
+        {
+            int bal1 = 0;
+            while (bal >= 0)
+            {
+                Console.WriteLine("Enter the amount that you want to debit: ");
+                int amount = int.Parse(Console.ReadLine());
+                if (amount <= bal)
+                {
+                    bal1 = bal - amount;
+                    Console.WriteLine("Amount debited successfully , remaining balence is :" + bal1);
+
+                }
+                else
+                {
+                    throw (new Balance("Insufficient Balence :"));
+                }
+                bal = bal - amount;
+            }
+        }
+    }
+    class Balance : Exception
+    {
+        public Balance(string Msg) : base(Msg)
+        {
+
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the amount you want to credit :");
+            int bal = int.Parse(Console.ReadLine());
+            Bank b = new Bank();
+            try
+            {
+                b.transaction(bal);
+
+            }
+            catch (Balance e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
