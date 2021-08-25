1efcda266475db8a003116111c31e0d562f0114d
suro99 <89373189+suro99@users.noreply.github.com>
2021-08-25 10:21:50 +0530

    Array(int-char)

---
+++ Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 65, 66, 67, 68, 69 };
+            char[] arr1 = new char[5];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr1[i] = (char)arr[i];
+                Console.Write(arr1[i] + "\t");
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
