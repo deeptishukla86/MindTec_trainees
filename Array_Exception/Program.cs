a6b12de61019032e08d75bba4aa9106712df4715
suro99 <89373189+suro99@users.noreply.github.com>
2021-08-25 14:33:20 +0530

    Array_Exception

--- Array_Exception/Program.cs
+++ Array_Exception/Program.cs
@@ -10,6 +10,26 @@ namespace Array_Exception
     {
         static void Main(string[] args)
         {
+            int[,] arr1 = new int[2, 2] { { 1, 2 }, {3 , 4 } };
+            int[] arr2 = new int[4];
+            int k = 0;
+            try
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                        arr2[k++] = arr1[i, j];
+                }
+                for(int i=0;i<4;i++)
+                {
+                    Console.Write(arr2[i]+"\t");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
