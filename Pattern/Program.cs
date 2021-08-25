81b47e7bd2d43f5065f53d13ac497b6460f76633
suro99 <89373189+suro99@users.noreply.github.com>
2021-08-25 09:14:53 +0530

    Pattern

---
+++ Pattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the range: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<=i;j++)
+                {
+                    Console.Write("*\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
