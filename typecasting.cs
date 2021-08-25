076f7a60bf5b2e81db1b63ba55e51aec5c12a808
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-26 00:47:45 +0530

    typecasting_example

---
+++ typecasting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typecasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[4]{ 65,66,67,68};
+            char[] c = new char[4];
+            for (int i = 0; i < 4; i++)
+            {
+                c[i] = (char)A[i];
+                Console.WriteLine(c[i]);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
