fc07a91360dcf882650b345eb14fedcb63485a32
Sujaan Chakraborty <sujaan.chakraborty@aot.edu.in>
2021-08-26 14:16:08 +0530

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,i;
+            Console.WriteLine("Enter any integer below 10");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x<=10)
+            {
+                Console.WriteLine("Using For Loop...");
+                for(i=1; i<=10; i++)
+                {
+                    Console.WriteLine(x + " x " + i + "=" + x * i);
+                }
+
+                Console.WriteLine("Using While Loop...");
+                i = 1;
+                while (i <= 10)
+                {
+                    Console.WriteLine(x + " x " + i + "=" + x * i);
+                    i++;
+                }
+
+                Console.WriteLine("Using Do While Loop...");
+                i = 1;
+                do
+                {
+                    Console.WriteLine(x + " x " + i + "=" + x * i);
+                    i++;
+                } while (i <= 10);
+            }
+            else
+            {
+                Console.WriteLine("Wrong Input! Close and Try again!");
+            }
+
+        }
+    }
+}
---
+++ areadll/areadll/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace areadll
+{
+    public class Class1
+    {
+        int radius, diagonal;
+        public string areaCircle()
+        {
+            double area;
+            Console.WriteLine("Enter radius of circle: ");
+            radius = Convert.ToInt32(Console.ReadLine());
+            area = 3.14 * radius * radius;
+            //Console.WriteLine(
+            return "Area of circle is " + area;
+            //);
+        }
+        public string areaRhombus()
+        {
+            double area;
+            Console.WriteLine("Enter radius of rhombus: ");
+            diagonal = Convert.ToInt32(Console.ReadLine());
+            area = diagonal * diagonal / 2.0;
+            //Console.WriteLine(
+            return "Area of rhombus is " + area;
+            //);
+        }
+    }
+}
