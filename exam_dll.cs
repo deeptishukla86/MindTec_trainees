92f6963279a7fbec99c41ac9a56ca8879b5a622a
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-27 00:17:53 +0530

    dll_console

---
+++ exam_dll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Class1 c = new Class1();
+            c.show();
+        }
+    }
+}
