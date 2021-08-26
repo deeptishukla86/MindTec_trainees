e95b6a6140bca2bcb27d7b082f5069d4913fc61a
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-27 00:36:39 +0530

    calculate area dll_console

---
+++ area_print_dll_console.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using calculatearea;
+
+namespace printarea_usingdll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 c = new Class2();
+            c.cir_area(7);
+            c.romb_area(2, 3);
+        }
+    }
+}
