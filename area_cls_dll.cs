71385e01dec18aab73e5eb703eb7e11d68518e77
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-27 00:25:36 +0530

    calculate area using dll_class

---
+++ area_cls_dll.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatearea
+{
+    public class Class2
+    {
+        public void cir_area(int r)
+        {
+            Console .WriteLine("area of circle: "+(3.17 * r * r)+" ");
+        }
+        public void romb_area(int d1,int d2)
+        {
+            Console.WriteLine("area of rhombus:" + ((d1 * d2) / 2));
+        }
+
+    }
+}
