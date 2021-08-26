1dbbec19b9cda57ba801f59c135bf1bbffe3cbb8
bhaskarpatra <bhaskapatra@gmail.com>
2021-08-26 12:06:31 +0530

    area of object add

---
+++ area_of_object/area_of_object/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using dll_area;
+
+namespace area_of_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the radius of the circle: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter the two diagonals of rombus: ");
+            double d1 = Convert.ToDouble(Console.ReadLine());
+            double d2 = Convert.ToDouble(Console.ReadLine());
+            var c = new Class1();
+            var r = new class2();
+            c.area_circle(radius);
+            r.area_rombus(d1, d2);
+        }
+    }
+}
---
+++ area_of_object/dll_area/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dll_area
+{
+    public class Class1
+    {
+        public  void area_circle(double radius ) 
+        {
+            double a1 = 3.14 * radius * radius;
+            Console.WriteLine("area of circle is: "+a1);
+        }
+       
+    }
+    public class class2
+    {
+        public void area_rombus(double d1, double d2)
+        {
+            double a2 = (d1 * d2)/2;
+            Console.WriteLine("area of rombus is :"+a2);
+        }
+    }
+}
