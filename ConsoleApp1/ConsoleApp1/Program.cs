d623f715ba1f0d9ea8fc58ca374b18711a7d238a
programmerchayan <chayan.officialmail@gmail.com>
2021-08-26 12:12:12 +0530

    Main file linked with Area dll file

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Area;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(5);
+            Rhombus rh = new Rhombus(10, 15);
+            c.circle_area();
+            rh.R_area();
+        }
+    }
+}
