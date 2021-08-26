4f464610070d2031e2caa4c097e07b6e323fadcb
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-27 01:15:10 +0530

    static_constant_variable

---
+++ static_constant_modify.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_constant_exmp
+{
+    class Program
+    {
+        int a;
+        static int b = 200;
+        const float pi = 3.14f;
+        readonly bool flag;
+        public Program(int a, bool flag)
+        {
+            this.a = a;
+            this.flag = flag;
+
+
+        }
+       
+        
+
+        static void Main(string[] args)
+        {
+            
+
+        
+            Console.WriteLine(Program.b);
+            Console.WriteLine(Program.pi);
+            Program p = new Program(50, true);
+            Program p1 = new Program(100, false);
+            Console.WriteLine(p.a + "" + p1.a);
+
+            Console.WriteLine(p.flag + "" + p1.flag);
+            p.a = 10;
+            b = 12;
+            Console.WriteLine("after modify:\n");
+            Console.WriteLine(p.a + " " + b);
+            
+
+
+        }
+    }
+
+
+}
