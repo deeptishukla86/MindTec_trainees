2c829d379ed784de8246b3817019565be2cc4139
thisisayush2912@gmail.com <thisisayush2912@gmail.com>
2021-08-26 12:15:29 +0530

    Day-4

---
+++ dllf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using test1;
+
+namespace dllf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test1.Class1 t = new Class1();
+            t.circle();
+            t.rhombus();
+        }
+    }
+}
