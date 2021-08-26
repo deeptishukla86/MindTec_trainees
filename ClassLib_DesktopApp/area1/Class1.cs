2370e9bbcc57df02a5ea26cff04675798c2c8196
suro99 <89373189+suro99@users.noreply.github.com>
2021-08-26 16:20:18 +0530

    ClassLib_DesktopApp

---
+++ ClassLib_DesktopApp/area1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    public class Class1
+    {
+        public double area;
+        public double cir(double n)
+        {
+            area = (3.14f * n * n);
+            return area;
+        }
+        public double rom(double m, double p)
+        {
+            area = (m * p)/2;
+            return area;
+        }
+
+    }
+}
---
+++ ClassLib_DesktopApp/area1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Circle;
+
+namespace area1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius of the circle");
+            double p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the diagonals of the rhombus");
+            double q = Convert.ToDouble(Console.ReadLine());
+            double r = Convert.ToDouble(Console.ReadLine());
+            Class1 c = new Class1();
+            Console.WriteLine("The Area Of Circle: " + c.cir(p));
+            Console.WriteLine("The Area Of Rhombus: " + c.rom(q,r));
+            Console.ReadLine();
+        }
+    }
+}
