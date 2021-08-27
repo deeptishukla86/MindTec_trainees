86bf2a9f5297aec36a6ff062c74dbda313664e6b
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-27 23:49:54 +0530

    stack_operations

---
+++ Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            s.Push(10);
+            s.Push("DXC");
+            s.Push(3.14f);
+            s.Push(true);
+            s.Push('D');
+
+            /*foreach (object o in s)
+            {
+                Console.WriteLine(o + "");
+
+            }
+            Console.WriteLine(s.Pop());
+            Console.WriteLine("  ");
+
+            foreach (object o in s)
+            {
+                Console.WriteLine(o + "");
+
+
+            }*/
+            int x;
+
+            x = s.Count;
+            Console.WriteLine(x);
+        }
+    }
+}
