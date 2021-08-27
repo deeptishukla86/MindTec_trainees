8a2d3261aa22cfd2d21ce7d8b59916472a671da1
suro99 <89373189+suro99@users.noreply.github.com>
2021-08-27 10:37:41 +0530

    ArrayList_EmpDetails

---
+++ ArrayList_EmpDetails/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ArrayList_EmpDetails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            Console.WriteLine("Enter Your Emp_Id: ");
+            int p = int.Parse(Console.ReadLine());
+            al.Add(p);
+            Console.WriteLine("Enter Your Name: ");
+            string a = Convert.ToString(Console.ReadLine());
+            al.Add(a);
+            Console.WriteLine("Enter Your Project Name: ");
+            string b = Convert.ToString(Console.ReadLine());
+            al.Add(b);
+            if (p > 1000 && p < 1006)
+            {
+                Console.WriteLine("You are in the project");
+                Console.WriteLine("Your project details is \nEmployee Id:{0}\nName:{1}\nProject Name:{2}", p, a, b);
+            }
+            else
+                Console.WriteLine("You are not in the project");
+            Console.Read();
+        }
+    }
+}
