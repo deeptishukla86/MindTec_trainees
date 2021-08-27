e5753da7ee45ae1181950b2f60c1b60f7bb45b6a
suro99 <89373189+suro99@users.noreply.github.com>
2021-08-27 15:49:52 +0530

    Sort_Icom1

---
+++ Sort_Icom1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Icom1
+{
+    class Program
+    {
+        public class SortByName : IComparer<Employee>
+        {
+            public int Compare(Employee x, Employee y)
+            {
+                return x.salary.CompareTo(y.salary);
+            }
+        }
+        public class Employee
+        {
+            public int emp_id { get; set; }
+            public string name { get; set; }
+            public double salary { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee()
+            {
+                emp_id = 100,
+                name = "Bifauxen",
+                salary = 20000
+            };
+            Employee emp2 = new Employee()
+            {
+                emp_id = 101,
+                name = "Mark",
+                salary = 10000
+            };
+            Employee emp3 = new Employee()
+            {
+                emp_id = 102,
+                name = "Warewolf",
+                salary = 30000
+            };
+            List<Employee> employees = new List<Employee>();
+            employees.Add(emp1);
+            employees.Add(emp2);
+            employees.Add(emp3);
+            Console.WriteLine("Employees before sorting salarywise\n");
+            foreach (var emp in employees)
+            {
+                Console.WriteLine("Employee Id: {0}", emp.emp_id);
+                Console.WriteLine("Employee Name: {0}", emp.name);
+                Console.WriteLine("Employee Salary: {0}", emp.salary);
+                Console.WriteLine();
+            }
+            SortByName sortByName = new SortByName();
+            employees.Sort(sortByName);
+            Console.WriteLine("Employees After sorting salarywise\n");
+            foreach (var emp in employees)
+            {
+                Console.WriteLine("Employee Id: {0}", emp.emp_id);
+                Console.WriteLine("Employee Name: {0}", emp.name);
+                Console.WriteLine("Employee Salary: {0}", emp.salary);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
