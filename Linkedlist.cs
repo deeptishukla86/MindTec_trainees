a1e8847df0994c2430d5c6c83ef3b1cef4f5c6c9
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-08-27 23:52:07 +0530

    Linkedlist_operations

---
+++ Linkedlist.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkedlist_concept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListNode<string> lst = new LinkedListNode<string>("Blue");
+            Console.WriteLine("After creating the node: ");
+            displayProperty(lst);
+            LinkedList<string> l = new LinkedList<string>();
+            l.AddLast(lst);
+            Console.WriteLine("After adding the linked list: ");
+            displayProperty(lst);
+        }
+        public static void displayProperty(LinkedListNode<string> list1)
+        {
+            if (list1.List == null)
+            {
+                Console.WriteLine("Node is not linked!");
+
+            }
+            else
+            {
+                Console.WriteLine("Node belongs to the Linked with 0th element!");
+            }
+            }
+        }
+}
