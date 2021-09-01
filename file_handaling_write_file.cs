230c8cd4e1083487da46d94124d60f27d827fdcb
Trishna1998 <89373220+Trishna1998@users.noreply.github.com>
2021-09-02 01:03:10 +0530

    write_file _file_handling

---
+++ file_handaling_write_file.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fill_handaling
+{
+    class Program
+    {
+        public void data()
+        {
+            StreamWriter sw = new StreamWriter("D://abc.txt");
+            Console.WriteLine("enter the data you want in text file");
+            string str = Console.ReadLine();
+            sw.WriteLine(str);
+            sw.Flush();
+            sw.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.data();
+        }
+    }
+}
